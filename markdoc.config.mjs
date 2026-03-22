a7c933ec8918c0191dca6901d327cf7ec6d6b931
Heru Handika <herubiolog@gmail.com>
2026-03-22 17:41:15 -0400

    Add full site guidelines.

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,6 +1,12 @@
-import { defineMarkdocConfig } from "@astrojs/markdoc/config";
+import { defineMarkdocConfig, component } from "@astrojs/markdoc/config";
 import starlightMarkdoc from "@astrojs/starlight-markdoc";
 
 export default defineMarkdocConfig({
   extends: [starlightMarkdoc()],
+  tags: {
+    carbonbadge: {
+      render: component("./src/components/WebCarbonBadge.astro"),
+      selfClosing: true,
+    },
+  },
 });
